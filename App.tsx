3943fbe8572aff6c71cb28ebca74bbfa7aafc3b6
skushagra <kushagra.rigel@gmail.com>
2023-10-27 23:54:27 +0530

    Started Attendance Application

---
+++ App.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable react-native/no-inline-styles */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <View>
+        <Text>hello world</Text>
+      </View>
+    </View>
+  );
+}
+
+export default App;
